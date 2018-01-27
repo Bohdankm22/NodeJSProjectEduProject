14236002a9a028a820069e94f25f3163b0b0b25c
Bohdankm22 <bohdansharipov@gmail.com>
2018-01-27 14:14:55 -0500

    Connected local mongodb to the nodejs server

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 'use strict';
 var http = require("http");
 var port = process.env.PORT || 1337;
+var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+
+// Connection URL to local MongoDB
+var url = 'mongodb://localhost:27017/courses';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    db.close();
+});
+
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
