5f2cb34b2e6b928aa7c2b5494c6f5d049548fd07
Bohdankm22 <bohdansharipov@gmail.com>
2018-02-10 23:01:14 -0500

    Added CORS.

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ mongoose.connect('mongodb://dbUser:dbUser2342!@138.197.163.67:27017/fishdb', fun
 
 });
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
