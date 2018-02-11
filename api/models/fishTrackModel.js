ff7da0cc8aaeb9158fc53d5be794a0bccdbd0309
Bohdankm22 <bohdansharipov@gmail.com>
2018-02-10 20:06:26 -0500

    Updated schema

--- api/models/fishTrackModel.js
+++ api/models/fishTrackModel.js
@@ -3,22 +3,48 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 
-var FishTrackSchema = new Schema({
-    name: {
-        type: String,
-        required: 'Kindly enter the name of the task'
-    },
-    Created_date: {
-        type: Date,
-        default: Date.now
-    },
-    status: {
-        type: [{
-            type: String,
-            enum: ['pending', 'ongoing', 'completed']
-        }],
-        default: ['pending']
+var FishTrackSchema = new Schema(
+    {
+        fishery: {
+            name: String,
+            location: String,
+            catch_time: Timestamp,
+            territory: String,
+            fish_type: String,
+            batch_size: {
+                value: Number,
+                unit: String
+            },
+            handling: String,
+            temperature: {
+                value: Number,
+                unit: String
+            }
+        },
+        logistics: {
+            name: String,
+            pick_up_time: Timestamp,
+            handling: String,
+            temperature: {
+                value: Number,
+                unit: String
+            }
+        },
+        retail: {
+            name: String,
+            received: Timestamp,
+            handling: String,
+            temperature: {
+                value: Number,
+                unit: String
+            },
+            expected_shelf_life: {
+                value: Number,
+                unit: String
+            },
+            best_before: Timestamp
+        }
     }
-});
+);
 
 module.exports = mongoose.model('FishTrack', FishTrackSchema);
